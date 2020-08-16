4e0f8c8b7cc668f3de7ce1f42b2a477c17c498e6
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2020-08-16 19:19:08 +0600

    Create 979A-Pizza, Pizza, Pizza!!!.cpp

---
+++ 979A-Pizza, Pizza, Pizza!!!.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    if(n==0) cout<<0;
+   else if(n%2==0) cout<<n+1;
+    else cout<<(n+1)/2;
+}
+//10000000000
