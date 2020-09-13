72bda0f7903ff7443356e7f035e761f90fa2ae07
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2020-09-13 10:53:35 +0600

    Create 1095A-Repeating Cipher.cpp

---
+++ 1095A-Repeating Cipher.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int n,i,c=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(i=0;i<n;i+=c)
+    {
+         cout<<s[i];
+         c++;
+    }
+
+
+}
