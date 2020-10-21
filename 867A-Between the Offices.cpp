4c19f9197ec2a8a53ae6909d86280af98183812b
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2020-10-21 17:34:57 +0600

    Create 867A-Between the Offices.cpp

---
+++ 867A-Between the Offices.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,c=0,f=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    if(s[0]=='S' && s[n-1]=='F')
+        cout<<"YES";
+    else cout<<"NO";
+}
