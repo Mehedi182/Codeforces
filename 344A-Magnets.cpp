841c4c6be047b424a45504623a3f113f61e9a21b
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2020-10-30 19:09:22 +0600

    Create 344A-Magnets.cpp

---
+++ 344A-Magnets.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int n,c=0;
+    string a,s;
+    a=="22";
+    cin>>n;
+    while(n--)
+    {
+        cin>>a;
+
+        if(s!=a)
+        {
+            c++;
+        }
+        s=a;
+    }
+    cout<<c;
+}
