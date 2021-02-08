70a6babac941ee2b05faba10efa0abfcdee53109
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2021-02-08 15:30:15 +0600

    Create 259A- Little Elephant and Chess.cpp

---
+++ 259A- Little Elephant and Chess.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int c=0,n=8;
+    string s;
+    while(n--){
+    cin>>s;
+    if(s=="WBWBWBWB" || s=="BWBWBWBW")
+    {
+        c++;
+    }
+
+
+}
+if(c==8) cout<<"YES";
+    else cout<<"NO";
+}
