2dee2ec8fee14ac116fd2750054988ae7ace872a
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2021-04-06 17:18:35 +0600

    Create 977A-Wrong Subtraction.cpp

---
+++ 977A-Wrong Subtraction.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int n,k,i;
+    cin>>n>>k;
+    for(i=1;i<=k;i++)
+    {
+                    if(n%10!=0)
+        {
+                n=n-1;
+        }
+        else
+        {
+           n=n/10;
+        }
+}
+cout<<n;
+}
